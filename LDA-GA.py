23aa239908b9e6508dc14f33c4ef45d4d93f0e8c
DinoDx <r.rapacciuolo1@studenti.unisa.it>
2021-05-19 21:02:21 +0200

    update LDA_GA.py

--- LDA-GA.py
+++ LDA-GA.py
@@ -2,6 +2,7 @@ from gensim import corpora
 from geneticalgorithm2 import geneticalgorithm2 as ga
 from geneticalgorithm2 import Crossover, Mutations, Selection
 import time,pickle, numpy as np
+from matplotlib.pyplot import cohere
 from gensim.models.coherencemodel import CoherenceModel
 from gensim.models.ldamodel import LdaModel
 
@@ -19,14 +20,14 @@ def main():
 
 
     def fitness(c):
-        ldamodel = LdaModel(corpus=corpus, id2word=dictionary, num_topics=int(c[0]),
-                            passes=int(c[1]), alpha=float(c[2]), eta=float(c[2]))
+        print(c)
+        ldamodel = LdaModel(corpus=corpus, id2word=dictionary, num_topics=int(c[0]), alpha=float(c[2]), eta=float(c[2]))
         coherence = CoherenceModel(model=ldamodel, texts=texts, dictionary=dictionary, coherence= 'c_v')
-        score = coherence.get_coherence()
-        return -score
+        return -(coherence.get_coherence())
 
 
-    varbounds = np.array([[2, 20], [10, 100], [0, 1], [0, 1]])
+    varbounds = np.array([[2, 20], [0, 1], [0, 1]])
+    var_types = np.array(['int', 'real', 'real'])
     alg_param = {'max_num_iteration': 100,
                 'population_size': pop_size,
                 'mutation_probability': mutation_prob,
@@ -37,7 +38,7 @@ def main():
                 'selection_type': Selection.roulette(),
                 'max_iteration_without_improv': 10}
 
-    model = ga(fitness, 4, function_timeout=60.0 , variable_type='real', variable_boundaries=varbounds, algorithm_parameters=alg_param)
+    model = ga(fitness, 3, function_timeout=100.0 , variable_type_mixed=var_types, variable_boundaries=varbounds, algorithm_parameters=alg_param)
     model.run()
 
     print("--- %s seconds ---" % (time.time() - start_time))
